690d9582c63f9c3f3efa9b55ebef367a231b92be
James Bach <james@satisfice.com>
2023-02-19 04:04:13 -0800

    first commit

---
+++ get_local_storage.js
@@ -0,0 +1,20 @@
+javascript: 
+(
+	()=>
+	{
+		function download(text, name, type) 
+		{  
+			var a = document.createElement("a"); 
+			var file = new Blob([text], {type: type});  
+			a.href = URL.createObjectURL(file);  
+			a.download = name;  
+			a.click()
+		}
+		const result = {};
+		for (var i = 0; i < localStorage.length; i++)
+		{
+			result[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
+		}
+		download(JSON.stringify(result),"LocalStorage","application/json")
+	}
+)();
