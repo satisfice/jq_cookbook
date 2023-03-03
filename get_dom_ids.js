bab12cffaf57667185db50e7f3e98ebe6bdb4377
James Bach <james@satisfice.com>
2023-03-03 00:52:46 -0800

    new bookmarklets!

---
+++ get_dom_ids.js
@@ -0,0 +1,37 @@
+javascript: 
+(
+	()=>
+	{
+		function download(text, name, type) 
+		{  
+			var a = document.createElement("a"); 
+			var file = new Blob([text], {type: type});  
+			a.href = URL.createObjectURL(file);  
+			a.download = name;  
+			a.click()
+		}
+		ids = document.querySelectorAll('*[id]');
+		const dict = {};
+		for (i=0;i<ids.length;i++)
+		{
+			if (!(dom[i].tagName in dict))
+			{
+				dict[dom[i].tagName] = {};
+			}
+			dict[dom[i].tagName][ids[i].attributes['id'].value] = 0;
+		}
+		const out = {};
+		for (i of Object.keys(dict).sort())
+		{
+			if (!(i in out))
+			{
+				out[i] = [];
+			}
+			for (v of Object.keys(dict[i]).sort())
+			{
+				out[i].push(v);
+			}
+		}
+		download(JSON.stringify(out,null,2),"DOM_IDs","application/json");
+	}
+)();
