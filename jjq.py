9325210c6c48a68385e57b1f664a063f64511ee8
James Bach <james@satisfice.com>
2025-04-04 09:23:50 -0700

    python and exe version

--- jjq.py
+++ jjq.py
@@ -1,5 +1,5 @@
 #  TODO:
-#  - mjq --walkthrough
+#  --walkthrough
 #      for each filter in a .jq file, the preceding comment(s), displays the filter string,
 #      and then runs that filter (including all prior filters)
 
@@ -13,27 +13,33 @@ import subprocess
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
+import argparse
 
-colorama_init()
+def get_executable_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)  # Returns path to the .exe
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
 
 def get_jqfiles():
     jjq_files = []
-    for filename in os.listdir():
-        # no need to display the extension in the list of available commands
+    for filename in os.listdir(scriptdir):
         if filename.endswith(".jq"):
-            jjq_files.append(filename.replace(".jq", ""))
+            jjq_files.append(filename)
     return jjq_files
 
-def display_help(jq_file):
+def fix_name(script):
     # if the user hasn't supplied ".jq", stick it on
-    if jq_file.endswith(".jq"):
-        f = open(jq_file, "r")
+    if script.endswith(".jq"):
+        return script
     else:
-        f = open(jq_file + '.jq.', "r")
+        return script + ".jq"
 
-    # No need to have the .jq on the command name.
-    jjq_command = jq_file.replace('.\\', '').replace(".jq", "")
-    print (f"\n{Fore.LIGHTBLUE_EX}{jjq_command}{Style.RESET_ALL}\n")
+def display_help(script):
+
+    print (f"\n{Fore.LIGHTBLUE_EX}{script.replace('.\\', '').replace(".jq", "")}{Style.RESET_ALL}\n")
+    
+    f = open(scriptdir + "/" + script, "r")
 
     help_text = f.readline().strip("\n")
     while re.match("^#", help_text):
@@ -41,55 +47,90 @@ def display_help(jq_file):
         print("  ", help_text.replace("#", ""))
         help_text = f.readline().strip("\n")
 
-def process_command():
-    if len(sys.argv) == 4:
-        command = f"jq -r -f {sys.argv[1] + '.jq'} --arg myvar {sys.argv[2]} {sys.argv[3]}"
+def process_command(script,target,input):
+    if input:
+        command = f"jq -r -f {script} --arg myvar {input} {target}"
     else:
-        command = f"jq -r -f {sys.argv[1] + '.jq'} {sys.argv[2]}"
-    print(command)
+        command = f"jq -r -f {script} {target}"
 
     subprocess.run(command, shell=True)
 
-# If there are no parameters at all, print the general help
-if len(sys.argv) == 1:
-    print("mjq - a caller for files that contain complex jq queries")
-    print("Usage: ")
-    print("     mjq.py")
-    print("         displays this message\n")
-    print("     mjq.py --list  (or --help, with no other parameters)")
-    print("         displays a list of available jq query files\n")
-    print("     mjq.py --help [jq_file]")
-    print("         provides help for the specified .jq file\n")
-    print("     mjq.py --walkthrough [jq_file]")
-    print("         provides help for the specified .jq file\n")
-    print("     mjq.py [jq_file] [parameters]")
-    print("         runs the specified jq files, passing the required parameters")
-    print("         example: mjq.py har-scan-get-json . my_harfile.har")
-    print("         example: mjq.py har-get-req 3 my_harfile.har")
-    print("         example: mjq.py collate-keys my_jsonfile.json")
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        prog='JJQ',
+        description='A wrapper around JQ to serve complex scripts.',
+        epilog='Written by James Bach and Michael Bolton',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
 
-# One argument  --help or --list produces a list of .jq scripts
-if len(sys.argv) == 2:
+    # Create a mutually exclusive group for --list and --listall
+    list_group = parser.add_mutually_exclusive_group()
+    list_group.add_argument(
+        "--list",
+        nargs="?",
+        const=True,  # Default value when --list is specified without an argument
+        metavar="SCRIPT",
+        help="List all available scripts, or get help on specific script"
+    )
+    list_group.add_argument(
+        "--listall",
+        action="store_true",
+        help="List all available scripts with capsule descriptions"
+    )
+    
+    # Add positional arguments that are only required if neither --list nor --listall is specified
+    parser.add_argument(
+        "script",
+        nargs="?",  # Make it optional initially
+        help="Script file to execute"
+    )
+    parser.add_argument(
+        "input",
+        nargs="?",  # Make it optional initially
+        help="Optional parameter for script"
+    )
+    parser.add_argument(
+        "json_file",
+        nargs="?",  # Make it optional initially
+        help="JSON file as target"
+    )
+    
+    args = parser.parse_args()
+    
+    # Validate arguments
+    if not args.list and not args.listall:
+        # In normal mode, script and json_file are required
+        if args.input is None:
+            parser.error("Please specify a JQ script and target JSON file.")
+        elif args.json_file is None:
+            args.json_file = args.input
+            args.input = None    
+    return args
+
+
+colorama_init()
+scriptdir = get_executable_path()
+
+args = parse_arguments()
+
+if args.list is not None or args.listall is not None:
     jjq_files = get_jqfiles()
-    if sys.argv[1] == "--list" or sys.argv[1] == "--help":
+
+if args.list is not None:
+    if args.list is True:  # --list specified without an argument
         print("Available jjq commands:")
         for file in jjq_files:
-            print(f"\t{file}")
+            print(f"\t{file.replace(".jq","")}")
+    else:  # --list specified with an argument
+        display_help(fix_name(args.list))
 
-    elif sys.argv[1] == "--listall" or sys.argv[1] == "--helpall":
-        for jq_file in jjq_files:
-            display_help(jq_file)
+elif args.listall:
+    print("Listing all items with detailed information")
+    for jq_file in jjq_files:
+        display_help(jq_file)
+else:
+    process_command(fix_name(scriptdir + "/" + args.script),args.json_file,args.input)
 
-if len(sys.argv) >= 3:
-    if sys.argv[1] == "--list" or sys.argv[1] == "--help":
-        display_help(sys.argv[2])
 
-    elif sys.argv[1] == "--walkthrough":
-        #TBD
-        pass
-    else:
-        process_command()
-
-exit()
-4:56 PM
 
